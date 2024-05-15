30355cb5c18222ebcc976e593af24c7375f0187e
Aulia Firda Syafira <auliafsyafira@gmail.com>
2024-05-15 20:38:08 +0700

    jobsheet 12 - E

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -18,8 +18,13 @@ class FileUploadController extends Controller
             'berkas'=>'required|file|image|max:5000',]);
             $extfile=$request->berkas->getClientOriginalName();
             $namaFile='web-'.time().".".$extfile;
-            $path = $request->berkas->storeAs('uploads', $namaFile);
+            $path = $request->berkas->storeAs('public', $namaFile);
+
+            $pathBaru=asset('storage/'.$namaFile);
             echo "proses upload berhasil, file berada di: $path";
+            echo "<br>";
+            echo "Tampilkan link:<a href='$pathBaru'>$pathBaru</a>";
+
             //echo $request->berkas->getClientOriginalName()."lolos validasi";
 
         // if($request->hasFile('berkas')){
